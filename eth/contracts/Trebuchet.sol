791a4ce9f0a1c791a553c97e6756f02e9121e433
nebojsa.urosevic <nebojsa94urosevic@gmail.com>
2019-08-25 10:23:42 +0200

    Ecrecover

--- eth/contracts/Trebuchet.sol
+++ eth/contracts/Trebuchet.sol
@@ -22,11 +22,28 @@ contract Trebuchet {
         for (uint i = 0; i < _data.length; i++) {
             bytes memory _datum = _data[i];
 
+            bytes32 r;
+            bytes32 s;
+            uint8 v;
+            address identity;
+        
+            assembly {
+                mstore(_datum, sub(mload(_datum), 65))
+                identity := shr(96, mload(add(_datum, 35)))
+
+                r := mload(add(_datum, mload(_datum)))
+                s := mload(add(_datum, add(mload(_datum), 32)))
+                v := byte(0, mload(add(_datum, add(mload(_datum), 64))))
+                
+            }
+            
+            require(ecrecover(keccak256(_datum), v, r, s) == identity);
+             
             assembly {
                 pop(
                     call(
                         shr(232, mload(add(_datum, 32))),
-                        shr(96, mload(add(_datum, 35))),
+                        identity,
                         0,
                         add(_datum, 55),
                         sub(mload(_datum), 23),
